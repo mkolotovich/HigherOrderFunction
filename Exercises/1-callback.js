1fcaa122c814372c00e2f2a0df86710bca0c5547
k2miha <k2punk@gmail.com>
2019-12-27 12:03:34 +0300

    make hw

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const iterate = (obj, callback) => null;
+const iterate = (obj, callback) => {
+  for (const key in obj) {
+    const value = obj[key];
+    // callback(key, value);
+    callback(key, value, obj);
+  }
+
+};
 
 module.exports = { iterate };
--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,32 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+// const contract = (...f) => {
+//   const args = [...f];
+//   console.log(args);
+//   let type;
+//   for (let i = 0; i < args.length; i++) {
+//     console.log(typeof args[i]);
+//     type = typeof args[i];
+//   }
+//   return (...args) => {
+//     console.log(f[0](...args));
+//     return f[0](...args);
+//   };
+// };
+const contract = (fn, ...types) => (...arr) => {
+  for (let i = 0; i < types.length - 1; i++) {
+    const argType = typeof arr[i];
+    const neededArgType = types[i].name.toLowerCase();
+    if (argType !== neededArgType) {
+      throw new TypeError('Types are different');
+    }
+  }
+  const neededReType = types[types.length - 1].name.toLowerCase();
+  const result = fn(...arr);
+  if (typeof result !== neededReType) {
+    throw new TypeError('Types are different');
+  }
+  return result;
+};
 
 module.exports = { contract };
